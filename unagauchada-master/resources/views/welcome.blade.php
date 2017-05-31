904d774e156c6ad60c290083803f8be32cf085df
Gonza <gonza_heavy@hotmail.com>
2017-05-31 19:52:17 -0300

    Tanteando la ramita.

--- unagauchada-master/resources/views/welcome.blade.php
+++ unagauchada-master/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    VIVA LA JODA
                 </div>
 
                 <div class="links">
