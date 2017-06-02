eb6feca81bc6807ad0534ff0ccee92fdf2c4a0e0
SartiniA <alessartini@gmail.com>
2017-06-02 15:59:07 -0300

    boton crear gauchada

--- unagauchada-master/resources/views/layouts/app.blade.php
+++ unagauchada-master/resources/views/layouts/app.blade.php
@@ -46,6 +46,7 @@
                             <li><a href="{{ route('login') }}">Loguearse</a></li>
                             <li><a href="{{ route('register') }}">Registrarse</a></li>
                         @else
+                            <li><a href="{{ route('create_gauchada_path') }}">Crear Gauchada</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
