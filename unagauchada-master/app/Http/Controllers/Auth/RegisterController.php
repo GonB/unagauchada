e8721ff9ef4492ab633bf076516ed80b1f526bc4
Bruno Balducchi <bruno.balducchi@gmail.com>
2017-06-09 15:40:36 -0300

    registro

--- unagauchada-master/app/Http/Controllers/Auth/RegisterController.php
+++ unagauchada-master/app/Http/Controllers/Auth/RegisterController.php
@@ -47,7 +47,6 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        $fecha = date('d-m-Y',strtotime('-18 years'));
         return Validator::make($data, [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
