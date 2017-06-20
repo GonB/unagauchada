86bb7aed86f93b8f13b6d9580b2b1d61613b84a4
Gonza <gonza_heavy@hotmail.com>
2017-06-20 12:59:48 -0300

    Naranja

--- unagauchada-master/resources/views/layouts/app.blade.php
+++ unagauchada-master/resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Una Gauchada') }}</title>
+    <title>Una gauchada</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -30,7 +30,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Una Gauchada') }}
+                        Menu Principal
                     </a>
                 </div>
 
