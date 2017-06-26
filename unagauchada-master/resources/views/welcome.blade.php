694d7569e4f8905e02daf9a4d7d333983420f38b
Gonza <gonza_heavy@hotmail.com>
2017-06-26 19:04:16 -0300

    Emprolijando el codigo

--- unagauchada-master/resources/views/welcome.blade.php
+++ unagauchada-master/resources/views/welcome.blade.php
@@ -10,75 +10,8 @@
         <link rel="shortcut icon" href="https://68.media.tumblr.com/avatar_28012e5b8492_128.png">        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <link rel="stylesheet" type="text/css" href="css/cssPrueba.css">
+        <link rel="stylesheet" type="text/css" href="css/cssPrincipal.css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: lavender;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0px;
-            }
-
-            .full-height {
-                height: 35vh;
-            }
-
-            .flex-center {
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: goldenrod;
-                padding: 0 25px;
-                font-size: 14px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-top: 300px;
-                margin-bottom: 30px;
-            }
-
-            .max{
-                width: 100%;
-            }
-
-            .contenedor-gauchadas{
-                margin: auto;
-                border-color: black;
-                border-radius: 2px;
-                text-align: center;
-            }
-
-
-
-        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
