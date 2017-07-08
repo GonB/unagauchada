0b19fda8ba5523092f126aba2ea467418ff568af
sartinia <alessartini@gmial.com>
2017-07-08 19:15:29 -0300

    despostular

--- unagauchada-master/resources/views/gauchada/index.blade.php
+++ unagauchada-master/resources/views/gauchada/index.blade.php
@@ -1,18 +1,42 @@
 @extends('layouts.app')
-
+  <?php 
+    use App\User;
+    use App\Postula;
+  ?>
 @section('content')
     @foreach($gauchada as $gauchada)
         @if($gauchada -> user_id == Auth::id())
+
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <a href="{{ route('gauchada_path', ['gauchada' => $gauchada]) }}">{{ $gauchada->titulo }}</a>
 
                     <small class="pull-right">
                         @if ($gauchada->activo)
+
+
                             <div><a href="{{ route('edit_gauchada_path', ['gauchada' => $gauchada->id]) }}" class="btn btn-info">Editar</a></div>
-                            <a href="{{ route('despostular_gauchada_path', ['gauchada' => $gauchada->id]) }}" class="btn btn-info">Despublicar</a>
+
+
+                            <?php
+                                $hay=False;
+                            ?>
+                            @foreach(Postula::all() as $post)
+                                @if (($post->gauchada_id == $gauchada->id) AND ($post->seleccionado == 1))
+                                    <?php 
+                                        $hay=True;
+                                    ?>
+                                @endif
+                            @endforeach
+                            @if($hay)
+                                <a class="btn btn-info">No Se puede Despublicar</a>
+                            @else
+                                <a href="{{ route('despostular_gauchada_path', ['gauchada' => $gauchada->id]) }}" class="btn btn-info">Despublicar</a>
+                            @endif
+                                
+
                             <form action="{{ route('delete_gauchada_path', ['gauchada' => $gauchada->id]) }}" method="GET">
-                                <button type="submit" class='btn btn-info'>Eliminar (error si hay postulados)</button>
+                                <button type="submit" class='btn btn-info'>Eliminar (error si existe postulados)</button>
                             </form>
                         @endif
                     </small>  
