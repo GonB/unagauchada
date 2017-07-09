8682684bdbd390f195b4dc458db572746af2727a
Gonza <gonza_heavy@hotmail.com>
2017-07-09 17:34:19 -0300

    ver perfil arreglado

--- unagauchada-master/resources/views/perfil/show.blade.php
+++ unagauchada-master/resources/views/perfil/show.blade.php
@@ -4,16 +4,20 @@
 
     <div class="container">
         <div class="panel panel-default">
-            <p>Nombre: {{ $user -> name }}</p>
-            <p>Nick: {{ $user -> nick }}</p>
-            <p>Fecha nacimiento: {{ $user -> birthdate }}</p>
-            <p>Email: {{ $user -> email }}</p>
-            <button class ="btn btn-warning" onclick="goBack()"> Atrás</button>
-            <script>
-                function goBack(){
-                    window.history.back();
-                }
-            </script>
+            <div class="panel-body">
+                <strong>
+                <p>Nombre: {{ $user -> name }}</p>
+                <p>Nick: {{ $user -> nick }}</p>
+                <p>Fecha nacimiento: {{ $user -> birthdate }}</p>
+                <p>Email: {{ $user -> email }}</p>
+                </strong>
+                <button class ="btn btn-warning" onclick="goBack()"> Atrás</button>
+                <script>
+                    function goBack(){
+                        window.history.back();
+                    }
+                </script>
+            </div>
         </div>
     </div>
     <hr>
