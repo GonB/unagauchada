1b31ba80793077e5c47f784f1ce19f1243986746
sartinia <alessartini@gmial.com>
2017-07-11 19:17:52 -0300

    categ

--- unagauchada-master/app/Http/Controllers/GauchadaController.php
+++ unagauchada-master/app/Http/Controllers/GauchadaController.php
@@ -51,19 +51,16 @@ class GauchadaController extends Controller
     public function store(Request $gauchada)
     {
         $user=User::find(Auth::id());
-        //return $gauchada['categoria'];
         if($user->credits>=1){
             $this->validate($gauchada, [
                 'titulo' => 'required|min:5',
                 'descripcion' => 'required|min:15',
                 'fecha_limite' => 'required|after:today'
             ]);
-            //$categ = CategoriaGauchada::where('titulo', 'LIKE','%'.$gauchada['categoria'].'%');
             Gauchada::create([
                 'user_id' => Auth::id(),
                 'titulo' => $gauchada['titulo'],
                 'descripcion' => $gauchada['descripcion'],
-                #AGREGAR CAMPO "CATEGORIA" A LAS GAUCHADAS
                 'categoria' => $gauchada['categoria'],
                 'fecha_limite' => $gauchada['fecha_limite'],
             ]);
