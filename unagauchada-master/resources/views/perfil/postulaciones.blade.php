f36141f87004cf0de498af5abadac079db8a5527
sartinia <alessartini@gmial.com>
2017-07-18 19:46:00 -0300

    ver mis postu

--- unagauchada-master/resources/views/perfil/postulaciones.blade.php
+++ unagauchada-master/resources/views/perfil/postulaciones.blade.php
@@ -8,13 +8,16 @@ use App\Gauchada;
 @section('content')
 	<div class="container">
         <div class="panel panel-default">
-            <div class="cab_form">Ver Mis Postulaciones</div>
+            <div class="cab_form">Mis Postulaciones</div>
             <div class="panel-body">
-                
+                @if ($mispostu->total() == 0)
+                    <strong>No se ha postulado en Gauchadas</strong>
+                @endif
                 @foreach($mispostu as $mispostu)
+                    
                 <strong>
                     <?php  
-                    $gau= Gauchada::find($mispostu->gauchada_id);
+                    $gau = Gauchada::find($mispostu->gauchada_id);
                     ?>
                     Gauchada: <a href="{{ route('gauchada_path', ['gauchada' => $gau]) }}">{{ $gau->titulo }}</a> <br>
                     Fecha : {{$mispostu->created_at->format('Y-m-d')}}</br>
